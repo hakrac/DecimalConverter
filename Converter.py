e1561a86be3573c6e6fb60368be1cb6c20146061
Hakim Rachidi <hakimrachidi02@gmail.com>
2017-08-11 22:34:01 +0200

    numbers like 10, 11,...15 will replace with a, b ...

--- Converter.py
+++ Converter.py
@@ -16,17 +16,53 @@ except ValueError:
 	print("Sorry, it has to be a number!")
 
 
+
+
+
 #calculation
 
 remainder = decimalint % baseint
+
+#print out letters for numeral system with a base of 10
+if baseint > 10:
+	if int(remainder) == 10:
+		remainder = "a"
+	elif int(remainder) == 11:
+		remainder = "b"
+	elif int(remainder) == 12:
+		remainder = "c"
+	elif int(remainder) == 13:
+		remainder = "d"
+	elif int(remainder) == 14:
+		remainder = "e"
+	elif int(remainder) == 15:
+		remiander = "f"
+
 y = decimalint / baseint
 decimalint = y
-result += str(int(remainder))
+result += str(remainder)
 
 while int(y) > 0:
 	remainder = decimalint % baseint
-	y = decimalint / baseint
+
+	if baseint > 10:
+		if int(remainder) == 10:
+			remainder = "a"
+		elif int(remainder) == 11:
+			remainder = "b"
+		elif int(remainder) == 12:
+			remainder = "c"
+		elif int(remainder) == 13:
+			remainder = "d"
+		elif int(remainder) == 14:
+			remainder = "e"
+		elif int(remainder) == 15:
+			remiander = "f"
+	else:
+		remainder = int(remainder)
+
+	y = int(decimalint / baseint)
 	decimalint = y
-	result += str(int(remainder))
+	result += str(remainder)
 
 print(decimal +" is {}".format(result[::-1]))
