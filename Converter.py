4e24cec92be0a795a5b28a091038247d94d5318f
Hakim Rachidi <hakimrachidi02@gmail.com>
2017-08-11 22:06:26 +0200

    User can set the base

--- Converter.py
+++ Converter.py
@@ -3,23 +3,30 @@ import sys
 result = ""
 
 decimal = input("Type in a new decimal number: ")
-try: 
+try:
 	decimalint = int(decimal)
 except ValueError:
 	print("Sorry, it has to be a number")
 	sys.exit()
 
+base = input("Type in the base of the numeral system: ")
+try:
+	baseint = int(base)
+except ValueError:
+	print("Sorry, it has to be a number!")
+
+
 #calculation
 
-remainder = decimalint % 2
-y = decimalint / 2 
+remainder = decimalint % baseint
+y = decimalint / baseint
 decimalint = y
 result += str(int(remainder))
 
 while int(y) > 0:
-	remainder = decimalint % 2
-	y = decimalint / 2
+	remainder = decimalint % baseint
+	y = decimalint / baseint
 	decimalint = y
-	result += str(int(remainder))	 
+	result += str(int(remainder))
 
 print(decimal +" is {}".format(result[::-1]))
