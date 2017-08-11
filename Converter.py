1e8b98351deba2bedc2026a1f19b4f7a2427555e
Hakim Rachidi <hakimrachidi02@gmail.com>
2017-08-11 22:53:56 +0200

    bug fixing: numeral system letters

--- Converter.py
+++ Converter.py
@@ -24,7 +24,23 @@ except ValueError:
 remainder = decimalint % baseint
 
 #print out letters for numeral system with a base of 10
-if baseint > 10:
+if baseint == 17:
+	if int(remainder) == 10:
+		remainder = "a"
+	elif int(remainder) == 11:
+		remainder = "b"
+	elif int(remainder) == 12:
+		remainder = "c"
+	elif int(remainder) == 13:
+		remainder = "d"
+	elif int(remainder) == 14:
+		remainder = "e"
+	elif int(remainder) == 15:
+		remiander = "f"
+	elif int(remainder) == 16:
+		remiander = "g"
+
+if baseint == 16:
 	if int(remainder) == 10:
 		remainder = "a"
 	elif int(remainder) == 11:
@@ -38,14 +54,61 @@ if baseint > 10:
 	elif int(remainder) == 15:
 		remiander = "f"
 
-y = decimalint / baseint
+elif baseint == 15:
+	if int(remainder) == 10:
+		remainder = "a"
+	elif int(remainder) == 11:
+		remainder = "b"
+	elif int(remainder) == 12:
+		remainder = "c"
+	elif int(remainder) == 13:
+		remainder = "d"
+	elif int(remainder) == 14:
+		remainder = "e"
+
+elif baseint == 14:
+	if int(remainder) == 10:
+		remainder = "a"
+	elif int(remainder) == 11:
+		remainder = "b"
+	elif int(remainder) == 12:
+		remainder = "c"
+	elif int(remainder) == 13:
+		remainder = "d"
+
+
+elif baseint == 13:
+	if int(remainder) == 10:
+		remainder = "a"
+	elif int(remainder) == 11:
+		remainder = "b"
+	elif int(remainder) == 12:
+		remainder = "c"
+
+
+
+elif baseint == 12:
+	if int(remainder) == 10:
+		remainder = "a"
+	elif int(remainder) == 11:
+		remainder = "b"
+
+
+elif baseint == 11:
+	if int(remainder) == 10:
+		remainder = "a"
+
+else:
+	remainder = int(remainder)
+
+y = int(decimalint / baseint)
 decimalint = y
 result += str(remainder)
 
 while int(y) > 0:
 	remainder = decimalint % baseint
 
-	if baseint > 10:
+	if baseint == 16:
 		if int(remainder) == 10:
 			remainder = "a"
 		elif int(remainder) == 11:
@@ -58,6 +121,51 @@ while int(y) > 0:
 			remainder = "e"
 		elif int(remainder) == 15:
 			remiander = "f"
+
+	elif baseint == 15:
+		if int(remainder) == 10:
+			remainder = "a"
+		elif int(remainder) == 11:
+			remainder = "b"
+		elif int(remainder) == 12:
+			remainder = "c"
+		elif int(remainder) == 13:
+			remainder = "d"
+		elif int(remainder) == 14:
+			remainder = "e"
+
+	elif baseint == 14:
+		if int(remainder) == 10:
+			remainder = "a"
+		elif int(remainder) == 11:
+			remainder = "b"
+		elif int(remainder) == 12:
+			remainder = "c"
+		elif int(remainder) == 13:
+			remainder = "d"
+
+
+	elif baseint == 13:
+		if int(remainder) == 10:
+			remainder = "a"
+		elif int(remainder) == 11:
+			remainder = "b"
+		elif int(remainder) == 12:
+			remainder = "c"
+
+
+
+	elif baseint == 12:
+		if int(remainder) == 10:
+			remainder = "a"
+		elif int(remainder) == 11:
+			remainder = "b"
+
+
+	elif baseint == 11:
+		if int(remainder) == 10:
+			remainder = "a"
+
 	else:
 		remainder = int(remainder)
 
